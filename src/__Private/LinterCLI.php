17cbdb9df13155c39583a9abbe77d6eaa4d4a869
Fred Emmott <fe@fb.com>
2017-09-21 13:58:34 -0700

    Add basic linter performance counters

--- src/__Private/LinterCLI.php
+++ src/__Private/LinterCLI.php
@@ -16,6 +16,14 @@ use namespace Facebook\HHAST\Linters;
 use namespace HH\Lib\{C, Dict, Str, Vec};
 
 final class LinterCLI extends CLIWithRequiredArguments {
+  private dict<string, float> $perfCounters = dict[];
+  private bool $profile = false;
+
+  private function bumpCounter(string $name, float $start, float $end): void {
+    $count = $this->perfCounters[$name] ?? 0.0;
+    $this->perfCounters[$name] = $count + ($end - $start);
+  }
+
   <<__Override>>
   public static function getHelpTextForRequiredArguments(): vec<string> {
     return vec['PATH'];
@@ -23,7 +31,13 @@ final class LinterCLI extends CLIWithRequiredArguments {
 
   <<__Override>>
   protected function getSupportedOptions(): vec<CLIOptions\CLIOption> {
-    return vec[];
+    return vec[
+      CLIOptions\flag(
+        () ==> { $this->profile = true; },
+        'Output performance counters when finished',
+        'profile',
+      ),
+    ];
   }
 
   private static function getLinterClasses(
@@ -36,21 +50,29 @@ final class LinterCLI extends CLIWithRequiredArguments {
     ];
   }
 
-  private static function lintFile(
+  private function lintFile(
     string $path,
   ): Traversable<Linters\LintError> {
     $errors = vec[];
     foreach (self::getLinterClasses() as $class) {
+      $start = microtime(true);
       $linter = new $class($path);
+      $errors = $linter->getLintErrors();
+      $end = microtime(true);
+      $this->bumpCounter($class.'#getLintErrors', $start, $end);
+
+      $start = microtime(true);
       $errors = Vec\concat(
         $errors,
-        self::processErrors($linter, $linter->getLintErrors()),
+        $this->processErrors($linter, $errors),
       );
+      $end = microtime(true);
+      $this->bumpCounter($class.'#processErrors', $start, $end);
     }
     return $errors;
   }
 
-  private static function lintDirectory(
+  private function lintDirectory(
     string $path,
   ): Traversable<Linters\LintError> {
     $it = new \RecursiveIteratorIterator(
@@ -63,20 +85,20 @@ final class LinterCLI extends CLIWithRequiredArguments {
       $ext = Str\lowercase($info->getExtension());
       if ($ext === 'hh' || $ext === 'php') {
         $file = $info->getPathname();
-        foreach(self::lintFile($file) as $error) {
+        foreach($this->lintFile($file) as $error) {
           yield $error;
         }
       }
     }
   }
 
-  private static function lintPath(
+  private function lintPath(
     string $path,
   ): Traversable<Linters\LintError> {
     if (is_file($path)) {
-      return self::lintFile($path);
+      return $this->lintFile($path);
     } else if (is_dir($path)) {
-      return self::lintDirectory($path);
+      return $this->lintDirectory($path);
     } else {
       printf(
         "'%s' doesn't appear to be a file or directory, bailing\n",
@@ -90,26 +112,36 @@ final class LinterCLI extends CLIWithRequiredArguments {
   public async function mainAsync(): Awaitable<int> {
     $had_errors = false;
     foreach ($this->getArguments() as $argument) {
-      $errors_in_arg = self::processArgument($argument);
-      $had_errors = $had_errors || $errors_in_arg;
+      $arg_is_clean = $this->processArgument($argument);
+      $had_errors = $had_errors || !$arg_is_clean;
     }
     if (!$had_errors) {
       print("No errors.\n");
     }
+    if ($this->profile) {
+      foreach ($this->perfCounters as $name => $value) {
+        printf("PERF %2.2fs %s\n", $value, $name);
+      }
+    }
     return $had_errors ? 2 : 0;
   }
 
-  private static function processArgument(string $argument): bool {
-    $errors = self::lintPath($argument);
+  private function processArgument(string $argument): bool {
+    $errors = $this->lintPath($argument);
     return C\first($errors) === null;
   }
 
-  private static function processErrors(
+  private function processErrors(
     Linters\BaseLinter $linter,
     Traversable<Linters\LintError> $errors,
   ): Traversable<Linters\LintError> {
+    $class = get_class($linter);
     $to_fix = vec[];
+    $error_start = microtime(true);
     foreach ($errors as $error) {
+      $error_end = microtime(true);
+      $this->bumpCounter($class.'#yieldError', $error_start, $error_end);
+
       $position = $error->getPosition();
       printf(
         "%s%s%s\n".
@@ -121,7 +153,13 @@ final class LinterCLI extends CLIWithRequiredArguments {
           ? $error->getFile()
           : sprintf('%s:%s:%s', $error->getFile(), $position[0], $position[1]),
       );
-      if ($error instanceof Linters\FixableLintError && $error->isFixable()) {
+
+      $start = microtime(true);
+      $fixable = $error instanceof Linters\FixableLintError && $error->isFixable();
+      $end = microtime(true);
+      $this->bumpCounter($class.'#isFixable', $start, $end);
+
+      if ($error instanceof Linters\FixableLintError && $fixable) {
         if (self::shouldFixLint($error)) {
           $to_fix[] = $error;
         } else {
@@ -131,10 +169,16 @@ final class LinterCLI extends CLIWithRequiredArguments {
         self::renderLintBlame($error);
         yield $error;
       }
+      $error_start = microtime(true);
     }
+    $error_end = microtime(true);
+    $this->bumpCounter($class.'#yieldError', $error_start, $error_end);
 
     if (!C\is_empty($to_fix)) {
+      $start = microtime(true);
       self::fixErrors($linter, $to_fix);
+      $end = microtime(true);
+      $this->bumpCounter($class.'#fix', $start, $end);
     }
   }
 
