c57ded27059f261b546e53cf89882d370c2378dd
Fred Emmott <fe@fb.com>
2017-09-22 16:00:29 -0700

    don't make travis so verbose

--- .travis.sh
+++ .travis.sh
@@ -8,9 +8,10 @@ curl https://getcomposer.org/installer | hhvm -d hhvm.jit=0 --php -- /dev/stdin 
 cd /var/source
 hhvm -d hhvm.php7.all=1 /usr/local/bin/composer install
 
-hh_server --check $(pwd)
+hh_client
+
 hhvm -d hhvm.php7.all=0 vendor/bin/phpunit tests/
 hhvm -d hhvm.php7.all=1 vendor/bin/phpunit tests/
 
-hhvm -d hhvm.php7.all=0 bin/hhast-lint -vv --perf
-hhvm -d hhvm.php7.all=1 bin/hhast-lint -vv --perf
+hhvm -d hhvm.php7.all=0 bin/hhast-lint --perf
+hhvm -d hhvm.php7.all=1 bin/hhast-lint --perf
