0af658be4a0120f03ec595cb48823f561f26abf5
Fred Emmott <fe@fb.com>
2017-09-29 10:31:01 -0700

    Don't attempt to migrate files that aren't hack
    fixes #9

--- src/__Private/MigrationCLI.php
+++ src/__Private/MigrationCLI.php
@@ -22,6 +22,12 @@ use type Facebook\HHAST\Migrations\{
 class MigrationCLI extends CLIWithRequiredArguments {
   use CLIWithVerbosityTrait;
 
+  const VERBOSE_SKIP_BECAUSE_GIT = 2;
+  const VERBOSE_SKIP_BECAUSE_NOT_HACK = 1;
+  const VERBOSE_SKIP_BECAUSE_VENDOR = 1;
+  const VERBOSE_MIGRATE = 2;
+  const VERBOSE_MIGRATE_NOT_HACK = 1;
+
   protected keyset<classname<BaseMigration>> $migrations = keyset[];
   private bool $includeVendor = false;
 
@@ -63,7 +69,18 @@ class MigrationCLI extends CLIWithRequiredArguments {
   final private function migrateFile(
     string $file,
   ): void {
-    $this->verbosePrintf(2, "Migrating file %s...\n", $file);
+    $this->verbosePrintf(
+      self::VERBOSE_MIGRATE,
+      "Migrating file %s...\n",
+      $file,
+    );
+    if (!self::isHackFile($file)) {
+      $this->verbosePrintf(
+        self::VERBOSE_MIGRATE_NOT_HACK,
+        "Migrating file %s despite it not looking like a Hack file\n",
+        $file,
+      );
+    }
     $ast = ast_from_file($file);
     foreach ($this->migrations as $migration) {
       $ast = (new $migration())->migrateAst($ast);
@@ -81,10 +98,26 @@ class MigrationCLI extends CLIWithRequiredArguments {
       }
       $file = $info->getPathname();
       if (!$this->includeVendor) {
+        if (Str\contains($file, '/.git/')) {
+          $this->verbosePrintf(
+            self::VERBOSE_SKIP_BECAUSE_GIT,
+            "Skipping file '%s' because it is in .git/\n",
+            $file,
+          );
+          continue;
+        }
         if (Str\contains($file, '/vendor/')) {
           $this->verbosePrintf(
-            1,
+            self::VERBOSE_SKIP_BECAUSE_VENDOR,
             "Skipping file '%s' because it is in vendor/\n",
+            $file,
+          );
+          continue;
+        }
+        if (!self::isHackFile($file)) {
+          $this->verbosePrintf(
+            self::VERBOSE_SKIP_BECAUSE_NOT_HACK,
+            "Skipping file '%s' because it is not a Hack file\n",
             $file,
           );
           continue;
@@ -122,5 +155,35 @@ class MigrationCLI extends CLIWithRequiredArguments {
       return 1;
     }
     return 0;
+  }
+
+  private static function isHackFile(string $file): bool {
+    static $cache = null;
+    if ($cache !== null) {
+      list($cache_file, $cache_result) = $cache;
+      if ($cache_file === $file) {
+        return $cache_result;
+      }
+    }
+
+    $f = fopen($file, 'r');
+    $prefix = fread($f, 4);
+    if ($prefix === '<?hh') {
+      $cache = tuple($file, true);
+      return true;
+    }
+
+    if (!Str\starts_with($prefix, '#!')) {
+      $cache = tuple($file, false);
+      return false;
+    }
+    rewind($f);
+    $_shebang = fgets($f);
+    $prefix = fread($f, 4);
+
+    $is_hh = $prefix === '<?hh';
+
+    $cache = tuple($file, $is_hh);
+    return $is_hh;
   }
 }
