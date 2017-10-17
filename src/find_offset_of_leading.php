63d047510cbb97a4c0a1ed4c18623bf51810715b
Bradley Davis <bradleyhd@users.noreply.github.com>
2017-10-17 10:27:47 -0700

    re-use stack while recursively calling find_offset_of_leading (#17)
    * re-use stack while recursively calling find_offset_of_leading
    
    * use Vec\take instead of array_pop

--- src/find_offset_of_leading.php
+++ src/find_offset_of_leading.php
@@ -17,6 +17,7 @@ use namespace HH\Lib\{C, Vec};
 function find_offset_of_leading(
   EditableNode $root,
   EditableNode $node,
+  ?vec<EditableNode> $stack = null
 ): int {
   if ($root === $node) {
     return 0;
@@ -24,7 +25,10 @@ function find_offset_of_leading(
 
   $parents = null;
   $found = false;
-  $stack = $root->findWithParents($it ==> $it === $node);
+
+  if ($stack === null) {
+    $stack = $root->findWithParents($it ==> $it === $node);
+  }
   invariant(
     !C\is_empty($stack),
     'did not find node in root',
@@ -33,7 +37,9 @@ function find_offset_of_leading(
     C\lastx($stack) === $node,
     'expected node at top of stack',
   );
-  $parent = $stack[C\count($stack) - 2];
+
+  $stack_count = C\count($stack);
+  $parent = $stack[$stack_count - 2];
 
   $within_parent = 0;
   foreach ($parent->getChildren() as $child) {
@@ -43,8 +49,11 @@ function find_offset_of_leading(
     $within_parent += $child->getWidth();
   }
 
+  $stack = Vec\take($stack, $stack_count - 1);
+
   return $within_parent + find_offset_of_leading(
     $root,
     $parent,
+    $stack
   );
 }
