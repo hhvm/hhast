2f93c1d20ce28444d9c68954c2ed6c3c2215bba7
Fred Emmott <fe@fb.com>
2018-02-08 15:08:59 -0800

    simplify travis config

--- .travis.sh
+++ .travis.sh
@@ -2,21 +2,15 @@
 set -ex
 hhvm --version
 
-wget https://getcomposer.org/composer.phar
-hhvm -d hhvm.php7.all=1 ./composer.phar install --ignore-platform-reqs
+composer install --ignore-platform-reqs
 
 hh_client
 
 vendor/bin/hh-codegen-verify-signatures codegen/
 
-hhvm -d hhvm.php7.all=0 vendor/bin/phpunit tests/
-hhvm -d hhvm.php7.all=1 vendor/bin/phpunit tests/
+hhvm vendor/bin/phpunit tests/
 
-hhvm -d hhvm.php7.all=0 bin/hhast-lint --perf
-hhvm -d hhvm.php7.all=1 bin/hhast-lint --perf
+hhvm bin/hhast-lint --perf
 
-HHVM_VERSION=$(hhvm --php -r 'echo HHVM_VERSION_ID;' 2>/dev/null);
-if [ $HHVM_VERSION -ge 32200 -a $HHVM_VERSION -lt 32300 ]; then
-  echo enable_experimental_tc_features = optional_shape_field, unknown_fields_shape_is_not_subtype_of_known_fields_shape >> .hhconfig
-  hh_server --check $(pwd)
-fi
+echo > .hhconfig
+hh_server --check $(pwd)
