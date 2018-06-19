a13107ac87bf0b8853c478de4fd382392e766920
Fred Emmott <fe@fb.com>
2018-06-19 13:01:33 -0700

    Add newline at end-of-file linter
    Also adds `BuiltLintError` to avoid needing to specialize for one-offs.

---
+++ tests/NewlineAtEndOfFileLinterTest.php
@@ -0,0 +1,27 @@
+<?hh // strict
+/*
+ *  Copyright (c) 2017-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the MIT license found in the
+ *  LICENSE file in the root directory of this source tree.
+ *
+ */
+
+namespace Facebook\HHAST;
+
+final class NewlineAtEndOfFileLinterTest extends TestCase {
+  use LinterTestTrait;
+
+  protected function getLinter(string $file): Linters\BaseLinter {
+    return new Linters\NewlineAtEndOfFileLinter($file);
+  }
+
+  public function getCleanExamples(): array<array<string>> {
+    return [
+      [ "<?hh\n" ],
+      [ "<?hh\nfoo();\n" ],
+      [ "<?hh\nfoo();\n/* hello */\n" ],
+    ];
+  }
+}
