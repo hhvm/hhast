a2638521cbbafc2d5e1f0648b5928469b7a67a31
Fred Emmott <fe@fb.com>
2018-11-30 09:40:23 -0800

    update hacktest and hh-clilib dependencies

--- .travis.sh
+++ .travis.sh
@@ -11,10 +11,3 @@ hhvm vendor/bin/hacktest tests/
 vendor/bin/hh-codegen-verify-signatures codegen/
 
 hhvm bin/hhast-lint
-
-# Make sure we pass when a release is required
-EXPORT_DIR=$(mktemp -d)
-git archive --format=tar -o "${EXPORT_DIR}/exported.tar" HEAD
-cd "$EXPORT_DIR"
-tar -xf exported.tar
-composer install --no-dev
